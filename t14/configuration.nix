ad2f8d59b8442bbd99d8a3a48b67a552f91a2490
Joeri Exelmans <joeri.exelmans@gmail.com>
2025-05-01 14:23:05 +0200

    update deps + disable xfce (too buggy)

--- t14/configuration.nix
+++ t14/configuration.nix
@@ -183,7 +183,7 @@
 
   # KDE config
   services.desktopManager.plasma6.enable = true;
-  services.xserver.desktopManager.xfce.enable = true;
+  #services.xserver.desktopManager.xfce.enable = true;
 
   # firmware updates
   services.fwupd.enable = true;
