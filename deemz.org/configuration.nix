864e73dc4131869f0200286d57d2e95fd7241ebe
Joeri Exelmans <joeri.exelmans@gmail.com>
2026-02-10 15:10:23 +0100

    ???

--- deemz.org/configuration.nix
+++ deemz.org/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, sops-nix, icomidal, ... }:
+{ config, system ? builtins.currentSystem, pkgs, sops-nix, icomidal, ... }:
 {
   imports =
     [ # Include the results of the hardware scan.
